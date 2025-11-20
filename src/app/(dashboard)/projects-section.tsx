3d81306438e4b73995591dbe84a6e573ffb335b7
Athena Cheng <athenachengyc15@gmail.com>
2025-11-20 13:41:46 +0800

    feat: implement of infinity project list

---
+++ src/app/(dashboard)/projects-section.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import {
+	AlertTriangle,
+	Copy,
+	File,
+	Loader,
+	MoreHorizontal,
+	Search,
+	Trash,
+} from "lucide-react";
+
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { formatDistanceToNow } from "date-fns/formatDistanceToNow";
+import { Button } from "@/components/ui/button";
+
+export const ProjectsSection = () => {
+	const router = useRouter();
+	const { data, status, isFetchingNextPage, hasNextPage, fetchNextPage } =
+		useGetProjects();
+
+	if (status === "pending") {
+		return (
+			<div className="space-y-4">
+				<h3>Recent Projects</h3>
+				<div className="flex flex-col gap-y-4 items-center justify-center h-32">
+					<Loader className="size-6 animate-spin text-muted-foreground" />
+				</div>
+			</div>
+		);
+	}
+
+	if (status === "error") {
+		return (
+			<div className="space-y-4">
+				<h3>Recent Projects</h3>
+				<div className="flex flex-col gap-y-4 items-center justify-center h-32">
+					<AlertTriangle className="size-6 text-muted-foreground" />
+					<p className="text-sm text-muted-foreground">
+						Failed to load projects
+					</p>
+				</div>
+			</div>
+		);
+	}
+
+	if (!data.pages.length) {
+		return (
+			<div className="space-y-4">
+				<h3>Recent Projects</h3>
+				<div className="flex flex-col gap-y-4 items-center justify-center h-32">
+					<Search className="size-6 text-muted-foreground" />
+					<p className="text-sm text-muted-foreground">No projects found</p>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className="space-y-4">
+			<h3 className="text-lg font-semibold">Recent Projects</h3>
+			<Table>
+				<TableBody>
+					{data.pages.map((group, i) => (
+						<React.Fragment key={i}>
+							{group.data.map((project) => (
+								<TableRow key={project.id}>
+									<TableCell
+										onClick={() => router.push(`/editor/${project.id}`)}
+										className="flex items-center gap-x-2 font-medium cursor-pointer"
+									>
+										<File className="size-6" />
+										{project.name}
+									</TableCell>
+									<TableCell className="hidden md:table-cell cursor-pointer">
+										{project.width}x{project.height} px
+									</TableCell>
+									<TableCell className="hidden md:table-cell cursor-pointer">
+										{formatDistanceToNow(project.updatedAt, {
+											addSuffix: true,
+										})}
+									</TableCell>
+									<TableCell className="flex items-center justify-end">
+										<DropdownMenu modal={false}>
+											<DropdownMenuTrigger asChild>
+												<Button disabled={false} size="icon" variant="ghost">
+													<MoreHorizontal className="size-4" />
+												</Button>
+											</DropdownMenuTrigger>
+											<DropdownMenuContent align="end" className="min-w-60">
+												<DropdownMenuItem
+													disabled={false}
+													onClick={() => {}}
+													className="h-10 cursor-pointer"
+												>
+													<Copy className="size-4 mr-2" />
+													Make a copy
+												</DropdownMenuItem>
+												<DropdownMenuItem
+													disabled={false}
+													onClick={() => {}}
+													className="h-10 cursor-pointer"
+												>
+													<Trash className="size-4 mr-2" />
+													Delete
+												</DropdownMenuItem>
+											</DropdownMenuContent>
+										</DropdownMenu>
+									</TableCell>
+								</TableRow>
+							))}
+						</React.Fragment>
+					))}
+				</TableBody>
+			</Table>
+			{hasNextPage && (
+				<div className="w-full flex items-center justify-center pt-4">
+					<Button
+						variant="ghost"
+						onClick={() => fetchNextPage()}
+						disabled={isFetchingNextPage}
+					>
+						Load More
+					</Button>
+				</div>
+			)}
+		</div>
+	);
+};
--- src/features/projects/api/use-update-project.ts
+++ src/features/projects/api/use-update-project.ts
@@ -27,6 +27,7 @@ export const useUpdateProject = (id: string) => {
 			return await response.json();
 		},
 		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["projects"] });
 			queryClient.invalidateQueries({ queryKey: ["project", { id }] });
 		},
 		onError: () => {
